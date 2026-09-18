0d1359a3a63ba45ff5e5dc4d05c2234dcfec2811
sudais122 <khansb17798@gmail.com>
2026-09-18 13:31:07 +0500

    setup:tailwind and react setup done

---
+++ Dashboard/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex flex-col items-left">
+      <h1 className="bg-sky-800 ">
+        Project Management Dashboard
+      </h1>
+
+      <p className="">
+        Tailwind CSS is working!
+      </p>
+    </div>
+  );
+}
+
+export default App;
